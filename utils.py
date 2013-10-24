ecc7a7a9250c2810e64a186eb0f023859978c78c
tangi <tangi@tangi.(none)>
2013-10-24 15:04:54 +0530

    added DataCenter class to contain Racks

--- utils.py
+++ utils.py
@@ -15,56 +15,45 @@ class VM(object):
     """Class to represent a virtual machine 
     inside a particular datacenter node 
     """
-    def __init__(self, rackid, vmid=None, ip=None):
+    vmids = []
+    def __init__(self, rackid, vmid):
         super(VM, self).__init__()
 
         if not isinstance(rackid,int):
             raise ValueError, "rackid has to be an integer"
 
+        self.vmid = vmid
         self.rackid = rackid
+        self.ip = self.getip()
 
-        if vmid is None:
-            pass
-            #create a random vmid          
-        
-        if ip is None:
-            pass
-            #random ip
 
-    def genrandip(self):
-        ipstr = ''
-        for i in range(4):
-            ipstr += str(choice(range(1,256)))+'.'
-        return ipstr[:-1]
+    def getip(self):
+        return '172.16'+'.'+str(self.rackid)+'.'+str(self.vmid)
 
 class Rack(object):
     """Class to represent a datacenter node
     """
-    def __init__(self, rackid=None):
-        super(Rack, self).__init__()
-        
-        if rackid is None:
-           #create a random rackid
-            rackid = randint(1,100)
- 
-        self.rackid = rackid    
-        self.vms = list()
 
-    def addvm(self, vm=None):
-        
-        if vm is None:
-            vm = VM(rackid=self.rackid)
+    def __init__(self,rackid):
+        self.rackid = rackid
+        self.vmids=[]
+    def addvm(self):
+        vmid = 0
+        if len(self.vmids) == 0:
+            vmid = 0
+        else:
+            vmid = self.vmids[-1]['vmid']+1
 
-        if not isinstance(vm,VM):
-            pass
-            #raise error
+        vm = VM(rackid=self.rackid, vmid=vmid)
 
         #add it to rack
-        tmp['vmid'] = vm.vmid
-        tmp['ip'] = vm.ip
-        tmp['rackid'] = vm.rackid
-        self.vms.append(tmp)
-    
+        vminfo = {}
+
+        vminfo['vmid'] = vm.vmid
+        vminfo['ip'] = vm.ip
+        vminfo['rackid'] = vm.rackid
+        self.vmids.append(vminfo)
+
     def delvm(self, vm): 
         if not isinstance(vm,VM):
             pass
@@ -73,10 +62,40 @@ class Rack(object):
         #if not vm in data:
             #raise
 
+    def __str__(self):
+        return self.vmids.__str__()
+
+class DataCenter(object):
+
+    def __init__(self):
+        self.rackids = []
+        super(DataCenter, self).__init__()
+
+    def addrack(self):
+        rackid = 0
+        if len(self.rackids) == 0:
+            rackid = 0
+            self.rackids.append(rackid)
+        else:
+            rackid = self.rackids[-1]+1
+            self.rackids.append(rackid)
+        rack = Rack(rackid)
+        return rack
+
+
 def tests():
-    tvm = VM(rackid=0)
+    d = DataCenter()
+    r1 = d.addrack()
     for i in range(10):
-        print tvm.genrandip()
+        r1.addvm()
+
+    r2 = d.addrack()
+    for i in range(10):
+        r2.addvm()
+
+    print r1
+    print '-'*80
+    print r2
        
 if __name__ == '__main__':
      tests() 
